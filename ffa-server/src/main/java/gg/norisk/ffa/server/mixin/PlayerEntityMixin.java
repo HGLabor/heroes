47669b94fb14a61793a5077a02cf08f0f94311cf
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2025-02-17 15:18:48 +0100

    feat: nerf axe damage

--- ffa-server/src/main/java/gg/norisk/ffa/server/mixin/PlayerEntityMixin.java
+++ ffa-server/src/main/java/gg/norisk/ffa/server/mixin/PlayerEntityMixin.java
@@ -1,18 +1,32 @@
 package gg.norisk.ffa.server.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import gg.norisk.ffa.server.mechanics.CombatTag;
 import gg.norisk.ffa.server.mechanics.KitEditor;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity implements CombatTag.ICombatPlayer {
+    @Shadow
+    @NotNull
+    public abstract ItemStack getWeaponStack();
+
+    @Shadow public abstract void remove(RemovalReason reason);
+
     @Unique
     private int ffaCombatTicks;
 
@@ -26,6 +40,23 @@ public abstract class PlayerEntityMixin extends LivingEntity implements CombatTa
             return constant;
         }
         return 1.18f;
+    }
+
+    @WrapOperation(
+            method = "attack",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getAttributeValue(Lnet/minecraft/registry/entry/RegistryEntry;)D", ordinal = 0)
+    )
+    private double axeDamageNerf(PlayerEntity instance, RegistryEntry<?> registryEntry, Operation<Double> original) {
+        var originalValue = original.call(instance, registryEntry);
+        if (KitEditor.INSTANCE.isUHC()) {
+            return originalValue;
+        } else {
+            if (getWeaponStack().isIn(ItemTags.AXES)) {
+                return originalValue / 4;
+            } else {
+                return originalValue;
+            }
+        }
     }
 
     @Override
