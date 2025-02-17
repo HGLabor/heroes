6979ec0baf9a78672a15df1139ad6fe9272d53c1
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2025-02-17 15:18:36 +0100

    feat: disable wither

---
+++ ffa-server/src/main/java/gg/norisk/ffa/server/mixin/WitherSkullBlockMixin.java
@@ -0,0 +1,18 @@
+package gg.norisk.ffa.server.mixin;
+
+import net.minecraft.block.WitherSkullBlock;
+import net.minecraft.block.entity.SkullBlockEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WitherSkullBlock.class)
+public abstract class WitherSkullBlockMixin {
+    @Inject(method = "onPlaced(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/SkullBlockEntity;)V", at = @At("HEAD"), cancellable = true)
+    private static void cancelWitherSpawning(World world, BlockPos pos, SkullBlockEntity blockEntity, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
